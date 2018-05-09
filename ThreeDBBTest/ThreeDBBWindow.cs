52a8fd0f3933c49808577d3551f1a77f50e48573
kohoutech <george@kohoutech.com>
2018-05-08 23:55:35 -0400

    created a new project for testing running a separate window for rendering 3D images

---
+++ ThreeDBBTest/ThreeDBBWindow.cs
@@ -0,0 +1,30 @@
+﻿//testing running a separate window for rendering 3D images
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ThreeDBBTest
+{
+    public partial class ThreeDBBWindow : Form
+    {
+        ThreeDCanvas canvas;
+
+        public ThreeDBBWindow()
+        {
+            InitializeComponent();
+
+            canvas = new ThreeDCanvas();
+            canvas.Location = new Point(0, 0);
+            canvas.Size = new Size(this.Width, this.Height);
+            this.Controls.Add(canvas);
+
+            canvas.InitCanvas();
+        }
+    }
+}
---
+++ ThreeDBBTest/ThreeDCanvas.cs
@@ -0,0 +1,31 @@
+﻿//testing running a separate window for rendering 3D images
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace ThreeDBBTest
+{
+    class ThreeDCanvas : Control
+    {
+        [DllImport("Xavier.DLL", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void RunTestWindow(IntPtr module);
+
+
+        public ThreeDCanvas()
+        {
+            BackColor = Color.LightBlue;
+        }
+
+        public void InitCanvas()
+        {
+            IntPtr module = Process.GetCurrentProcess().Handle;
+            RunTestWindow(module);
+        }
+    }
+}
